1b2fb9ee854e10bc33b8aa13e8007624ea6346e6
zahoor <zahoor.uk.edu@gmail.com>
2025-11-29 22:59:27 +0000

    Add text content to AboutPage body

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -9,6 +9,30 @@ class AboutPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('About Us'),
       ),
-      body: const SizedBox(),
-    );
-  }
+      body: const Padding(
+  padding: EdgeInsets.all(20),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        'About the Union Shop',
+        style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+      ),
+      SizedBox(height: 20),
+      Text(
+        'The University of Portsmouth Students’ Union Shop provides official merchandise, clothing, gifts and essential items for students.',
+        style: TextStyle(fontSize: 16),
+      ),
+      SizedBox(height: 16),
+      Text(
+        'Our mission is to offer high-quality products at affordable prices while supporting student life on campus.',
+        style: TextStyle(fontSize: 16),
+      ),
+      SizedBox(height: 16),
+      Text(
+        'All profits made through the shop go back into supporting student activities and services.',
+        style: TextStyle(fontSize: 16),
+      ),
+    ],
+  ),
+),
