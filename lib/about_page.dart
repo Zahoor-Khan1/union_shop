0255e796427f7fc1f9b4a706c8b03c5ca53544d7
zahoor <zahoor.uk.edu@gmail.com>
2025-11-29 18:08:46 +0000

    Created the about_page in lib.

---
+++ lib/about_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About Us'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'About the Union Shop',
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'The University of Portsmouth Students’ Union Shop provides official merchandise, clothing, gifts and essential items for students.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Our mission is to offer high-quality products at affordable prices while supporting student life on campus.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'All profits made through the shop go back into supporting student activities and services.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
