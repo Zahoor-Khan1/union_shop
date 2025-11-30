3e4f9032ab14b4f8268fc958af065d37a5d43a23
zahoor <zahoor.uk.edu@gmail.com>
2025-11-30 20:41:46 +0000

    add first category (Hoodies) to Collections page

--- lib/collections_page.dart
+++ lib/collections_page.dart
@@ -3,20 +3,29 @@ import 'package:flutter/material.dart';
 class CollectionsPage extends StatelessWidget {
   const CollectionsPage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Collections'),
       ),
-      body:const Center(
-  child: Text(
-    'Shop by Category',
-    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-  ),
-),
-
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Shop by Category',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ListTile(
+              title: Text('Shirts'),
+              trailing: Icon(Icons.arrow_forward_ios, size: 16),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
