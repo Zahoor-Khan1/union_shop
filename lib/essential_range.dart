4e6e0d75ef9afb60de6c3240d378982c79a6b12d
zahoor <zahoor.uk.edu@gmail.com>
2025-12-01 17:11:43 +0000

    Add build method to EssentialRange Page

--- lib/essential_range.dart
+++ lib/essential_range.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class EssentialRangePage extends StatelessWidget {
   const EssentialRangePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
 }
