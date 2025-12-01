fe1ee89f7590f54f57f6d6c98c22f6502539bd02
zahoor <zahoor.uk.edu@gmail.com>
2025-12-01 13:15:40 +0000

    Add build method to Graduation Page

--- lib/graduation_page.dart
+++ lib/graduation_page.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 
 class GraduationPage extends StatelessWidget {
   const GraduationPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
 }
