f4032c927eb261e520b3ec5eefce076ebeed2a55
zahoor <zahoor.uk.edu@gmail.com>
2025-12-01 19:00:52 +0000

    Add build method to PridePage

--- lib/pride_page.dart
+++ lib/pride_page.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class PridePage extends StatelessWidget {
   const PridePage({super.key});
+ @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
 }
