1f4ee9188bb4456ba7459d673a1d7889d074cd91
zahoor <zahoor.uk.edu@gmail.com>
2025-12-01 21:50:19 +0000

    Add build method to Election discount Page

--- lib/election discounts_page.dart
+++ lib/election discounts_page.dart
@@ -2,4 +2,10 @@ import 'package:flutter/material.dart';
 
 class DiscountPage extends StatelessWidget {
   const DiscountPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
 }
