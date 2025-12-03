a291458a1be28a4857099432a399ca63d9cd0afc
zahoor <zahoor.uk.edu@gmail.com>
2025-12-03 21:42:20 +0000

    Update SalePage layout and content for improved user engagement. By moving the SALE page title to the center as like the orignal website

--- lib/sale_page.dart
+++ lib/sale_page.dart
@@ -7,10 +7,32 @@ class SalePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sale Collection'),
+        title: const Text('SALE'),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Sale items will appear here'),
+      body: const SingleChildScrollView(
+  child: Center(
+    child: Padding(
+
+          padding: EdgeInsets.only(top: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:const[ 
+            Text(
+                "Don't miss out! Get yours before they're all gone!",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                'All prices shown are inclusive of the discount. 🛒',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ),
       ),
     );
   }
