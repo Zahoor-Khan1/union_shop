5cf4b91ded81ecaea81289278eab40653c6311ec
zahoor <zahoor.uk.edu@gmail.com>
2025-12-03 12:40:23 +0000

    Added build method to the sales class

--- lib/sale_page.dart
+++ lib/sale_page.dart
@@ -2,3 +2,16 @@ import 'package:flutter/material.dart';
 
 class SalePage extends StatelessWidget {
   const SalePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sale Collection'),
+      ),
+      body: const Center(
+        child: Text('Sale items will appear here'),
+      ),
+    );
+  }
+}
