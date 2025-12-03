63289381fb2d2e186a9c036107e48b2cff255c37
zahoor <zahoor.uk.edu@gmail.com>
2025-12-03 22:38:49 +0000

    Add empty GridView layout for Sale products

--- lib/sale_page.dart
+++ lib/sale_page.dart
@@ -11,30 +11,36 @@ class SalePage extends StatelessWidget {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-  child: Center(
-    child: Padding(
-
-          padding: EdgeInsets.only(top: 40),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:const[ 
-            Text(
-                "Don't miss out! Get yours before they're all gone!",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 12),
-              const Text(
-                'All prices shown are inclusive of the discount. 🛒',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16),
-              ),
-              SizedBox(height: 32),
-
-            ],
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.only(top: 40),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Text(
+                  "Don't miss out! Get yours before they're all gone!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 12),
+                const Text(
+                  'All prices shown are inclusive of the discount. 🛒',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 32),
+                GridView.count(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: const [],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
