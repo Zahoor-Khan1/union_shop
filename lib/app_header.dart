73a065c507dd81f4275cd74a98941266bc052852
zahoor <zahoor.uk.edu@gmail.com>
2025-12-04 20:24:29 +0000

    Add reusable AppHeader widget for consistent layout across pages

---
+++ lib/app_header.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppHeader extends StatelessWidget {
+  const AppHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      color: Colors.white,
+    );
+  }
+}
