0422e216f5e3273a08e9d542d5440f78826304b4
zahoor <zahoor.uk.edu@gmail.com>
2025-12-04 20:36:52 +0000

    Add top purple banner to AppHeader

--- lib/app_header.dart
+++ lib/app_header.dart
@@ -6,8 +6,23 @@ class AppHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      color: Colors.white,
-    );
+  height: 100,
+  color: Colors.white,
+  child: Column(
+    children: [
+      Container(
+        width: double.infinity,
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        color: const Color(0xFF4d2963),
+        child: const Text(
+          'BIG SALE! OUR ESSENTIAL RANGE HAS DROPPED IN PRICE!',
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    ],
+  ),
+);
   }
 }
+
