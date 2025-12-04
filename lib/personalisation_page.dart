13d8d933790a5a7726842aa04a27acaabf1a062e
zahoor <zahoor.uk.edu@gmail.com>
2025-12-04 15:25:28 +0000

    Add body layout to PersonalisationPage with scrollable content

--- lib/personalisation_page.dart
+++ lib/personalisation_page.dart
@@ -9,7 +9,16 @@ class PersonalisationPage extends StatelessWidget {
   appBar: AppBar(
     title: const Text('Personalisation'),
     centerTitle: true,
+  ),body: SingleChildScrollView(
+  child: const Padding(
+    padding: EdgeInsets.all(24),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [],
+    ),
   ),
+),
+
 );
 
   }
