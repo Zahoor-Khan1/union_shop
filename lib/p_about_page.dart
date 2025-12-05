cf6379e1bb351c79fb478a3940dd09c4cb432da6
zahoor <zahoor.uk.edu@gmail.com>
2025-12-05 09:54:22 +0000

    Create empty Print Shack About page

---
+++ lib/p_about_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PrintShackAboutPage extends StatelessWidget {
+  const PrintShackAboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Print Shack About Page'),
+      ),
+    );
+  }
+}
