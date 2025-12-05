311ed21fb9282289015c21c5ab241be19c4a6668
zahoor <zahoor.uk.edu@gmail.com>
2025-12-05 05:22:28 +0000

    Add basic CartItem model and global cart list

---
+++ lib/cart.dart
@@ -0,0 +1,13 @@
+class CartItem {
+  final String name;
+  final String price;
+  final String image;
+
+  CartItem({
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+}
+
+List<CartItem> cartItems = [];
