6a4447f27167b44a44e92626e5794f37ee8d9932
zahoor <zahoor.uk.edu@gmail.com>
2025-12-16 22:58:05 +0000

    Add centered card layout to authentication page

--- lib/views/authentication_page.dart
+++ lib/views/authentication_page.dart
@@ -5,10 +5,27 @@ class AuthenticationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
       body: Center(
-        child: Text('Authentication Page'),
+        child: Container(
+          width: 420,
+          padding: const EdgeInsets.all(32),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: const SizedBox(),
+        ),
       ),
     );
   }
 }
+
