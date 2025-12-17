d44d15231947c8abaed9c1cac34ea32cb764e14b
zahoor <zahoor.uk.edu@gmail.com>
2025-12-17 07:54:09 +0000

    Add static product list for search

--- lib/views/search_results_page.dart
+++ lib/views/search_results_page.dart
@@ -8,6 +8,23 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+final List<Map<String, String>> products = [
+  {
+    'name': 'Classic Hoodie',
+    'image': 'assets/images/Classic Sweatshirt.png',
+    'price': '£15.00',
+  },
+  {
+    'name': 'Essential Hoodie',
+    'image': 'assets/images/zip_hoodie.png',
+    'price': '£14.00',
+  },
+  {
+    'name': 'Signature Hoodie',
+    'image': 'assets/images/signature hoodie.png',
+    'price': '£32.99',
+  },
+];
 
 
   @override
