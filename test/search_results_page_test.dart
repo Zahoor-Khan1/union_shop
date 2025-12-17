40021640224217ec17820bc6f201d1bd8b81208f
zahoor <zahoor.uk.edu@gmail.com>
2025-12-17 11:10:45 +0000

    remove unused search results import

--- test/search_results_page_test.dart
+++ test/search_results_page_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:union_shop/views/search_page.dart';
 import 'package:union_shop/views/search_results_page.dart';
 
 void main() {
